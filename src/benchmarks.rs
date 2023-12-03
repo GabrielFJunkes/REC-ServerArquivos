a2122c454902905a04c2df65aea120aa42cb6495
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2023-12-03 20:44:13 -0300

    feat: benchmark

--- src/benchmarks.rs
+++ src/benchmarks.rs
@@ -15,7 +15,7 @@ fn run_benchmark_for_2_params(f: fn(&mut TcpStream, &str, bool), param: &str, da
     f(&mut stream, param, false);
     
     let elapsed = now.elapsed();
-    data.lock().unwrap().push(elapsed.as_millis());
+    data.lock().unwrap().push(elapsed.as_micros());
 }
 
 fn upload_benchmark(id: u64, data: Arc<Mutex<Vec<u128>>>) {
@@ -41,9 +41,6 @@ fn delete_benchmark(id: u64, data: Arc<Mutex<Vec<u128>>>) {
 fn run_benchmark(count: u64, f: fn(u64, Arc<Mutex<Vec<u128>>>), data: Arc<Mutex<Vec<u128>>>, nome: &str) {
     let mut threads = Vec::<Result<JoinHandle<()>, ()>>::new();
     for i in 0..count {
-        if i%250==0{
-            thread::sleep(Duration::from_millis(400));
-        }
         let data1 = data.clone();
         threads.push(thread::Builder::new().name(format!("{nome} - {count}")).spawn(move || {
             f(i, data1);
@@ -60,47 +57,23 @@ fn run_benchmark(count: u64, f: fn(u64, Arc<Mutex<Vec<u128>>>), data: Arc<Mutex<
 
 fn run_benchmark_print_average(f: fn(u64, Arc<Mutex<Vec<u128>>>), nome: &str, count: u64) {
     let data = Arc::new(Mutex::new(Vec::new()));
-    run_benchmark(count, f, Arc::clone(&data), nome);
-    let lock = data.lock().unwrap();
-    let sum: u128 = lock.iter().sum();
-    let average = sum as f64 / count as f64;
-    println!("Média {nome}: {:.2} ms", average)
+    let mut loop_count = 1;
+    loop {
+        run_benchmark(count, f, Arc::clone(&data), nome);
+        std::process::Command::new("clear").status().unwrap();
+        let lock = data.lock().unwrap();
+        let iter = lock.iter();
+        let sum: u128 = iter.clone().sum();
+        let average = sum as f64 / (count * loop_count) as f64;
+        println!("Média de {count} {nome}: {:.2} μs - max: {} μs - min: {} μs", average, iter.clone().max().unwrap(), iter.min().unwrap());
+        loop_count += 1;
+    }
 }
 
 fn main() {
-    let count = 10;
-    println!("\nQuantidade de clientes: {count}");
-    run_benchmark_print_average(upload_benchmark, "upload", count);
-    run_benchmark_print_average(download_benchmark, "download", count);
-    run_benchmark_print_average(delete_benchmark, "delete", count);
-
-    let count = 100;
-    println!("\nQuantidade de clientes: {count}");
-    run_benchmark_print_average(upload_benchmark, "upload", count);
-    run_benchmark_print_average(download_benchmark, "download", count);
-    run_benchmark_print_average(delete_benchmark, "delete", count);
-
-    thread::sleep(Duration::from_millis(500));
-
     let count = 300;
-    println!("\nQuantidade de clientes: {count}");
-    run_benchmark_print_average(upload_benchmark, "upload", count);
+    //run_benchmark_print_average(upload_benchmark, "upload", count);
     run_benchmark_print_average(download_benchmark, "download", count);
-    run_benchmark_print_average(delete_benchmark, "delete", count);
+    //run_benchmark_print_average(delete_benchmark, "delete", count);
 
-    thread::sleep(Duration::from_millis(500));
-
-    let count = 500;
-    println!("\nQuantidade de clientes: {count}");
-    run_benchmark_print_average(upload_benchmark, "upload", count);
-    run_benchmark_print_average(download_benchmark, "download", count);
-    run_benchmark_print_average(delete_benchmark, "delete", count);
-
-    thread::sleep(Duration::from_millis(500));
-
-    let count = 1000;
-    println!("\nQuantidade de clientes: {count}");
-    run_benchmark_print_average(upload_benchmark, "upload", count);
-    run_benchmark_print_average(download_benchmark, "download", count);
-    run_benchmark_print_average(delete_benchmark, "delete", count);
 }
